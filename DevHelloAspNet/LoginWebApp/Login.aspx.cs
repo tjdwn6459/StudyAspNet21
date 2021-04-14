84cfb96fb833ae8fcc99db591959bd7bccf0ee94
tjdwn6459 <tjdwn6459@naver.com>
2021-04-14 17:38:15 +0900

    210414 오늘의 커밋

---
+++ DevHelloAspNet/LoginWebApp/Login.aspx.cs
@@ -0,0 +1,44 @@
+﻿using LoginWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginWebApp
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnLogin_Click(object sender, EventArgs e)
+        {
+            var repo = new Repository();
+            if (repo.IsCorrectUser(TxtUserID.Text, TxtPassword.Text))
+            {
+                //성공
+                //인증부여
+                if(!string.IsNullOrEmpty(Request["ReturnUrl"]))
+                {
+                    FormsAuthentication.RedirectFromLoginPage(TxtUserID.Text, false);
+                }
+                else
+                {
+                    FormsAuthentication.SetAuthCookie(TxtUserID.Text, false); //인증된 시용자 아이디를 쿠키에 넣다
+                    Response.Redirect("~/Welcome.aspx");//Welcome로 옮기다
+                }
+            }
+            else
+            {
+                //실패
+                Response.Write("<script>alert('잘못된사용자 입니다.');</script>");
+                Response.End();
+            }
+        }
+    }
+}
---
+++ DevHelloAspNet/TestDB_Database/dbo/Stored Procedures/ListMemo.sql
@@ -0,0 +1,4 @@
+﻿CREATE Proc [dbo].[ListMemo]
+As
+	Select Num, Name, Email, Title, PostDate, PostIP
+	From Memos Order By Num Desc
